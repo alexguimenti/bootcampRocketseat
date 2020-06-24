e7b282389a2f18ff319e00ef65b6622ed422744d
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-24 20:23:18 -0300

    level 02 1st chalenge missing services only

--- Level 02/challenges/gostack-fundamentos-node/src/repositories/TransactionsRepository.ts
+++ Level 02/challenges/gostack-fundamentos-node/src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -6,23 +7,76 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
-    this.transactions = [];
+    this.transactions = [
+      {
+        id: uuid(),
+        title: 'Salário',
+        value: 4000,
+        type: 'income',
+      },
+      {
+        id: uuid(),
+        title: 'Pagamento da fatura',
+        value: 4000,
+        type: 'outcome',
+      },
+    ];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const incomeValues = incomeTransactions.map(
+      transaction => transaction.value,
+    );
+
+    const totalIncome = incomeValues.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const outcomeValues = outcomeTransactions.map(
+      transaction => transaction.value,
+    );
+
+    const totalOutcome = outcomeValues.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+    );
+
+    const total = totalIncome - totalOutcome;
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- Level 02/challenges/gostack-fundamentos-node/src/routes/transaction.routes.ts
+++ Level 02/challenges/gostack-fundamentos-node/src/routes/transaction.routes.ts
@@ -1,88 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
-interface Transaction {
-  id: string,
-  title: string;
-  value: number;
-  type: string;
-}
-
-const transactions: Transaction[] = [
-  {
-    id: uuid(),
-    title: "Salário",
-    value: 4000,
-    type: "income"
-  },
-  {
-    id: uuid(),
-    title: "Freela",
-    value: 2000,
-    type: "income"
-  },
-  {
-    id: uuid(),
-    title: "Pagamento da fatura",
-    value: 4000,
-    type: "outcome"
-  },
-  {
-    id: uuid(),
-    title: "Cadeira Gamer",
-    value: 1200,
-    type: "outcome"
-  }
-];
+// interface Transaction {
+//   id: string;
+//   title: string;
+//   value: number;
+//   type: string;
+// }
 
 transactionRouter.get('/', (request, response) => {
   try {
-    const incomeTransactions = transactions.filter(
-      transaction => transaction.type == 'income'
-    )
 
-    const incomeValues = incomeTransactions.map(
-      transaction => transaction.value
-    )
+    const transactions = transactionsRepository.all();
 
-    const totalIncome = incomeValues.reduce(
-      (accumulator, currentValue) => accumulator + currentValue
-    )
+    const balance = transactionsRepository.getBalance();
 
-    const outcomeTransactions = transactions.filter(
-      transaction => transaction.type == 'outcome'
-    )
-
-    const outcomeValues = outcomeTransactions.map(
-      transaction => transaction.value
-    )
-
-    const totalOutcome = outcomeValues.reduce(
-      (accumulator, currentValue) => accumulator + currentValue
-    )
-
-    const total = totalIncome - totalOutcome;
-
-    const balance = {
-      income: totalIncome,
-      outcome: totalOutcome,
-      total
-    }
-
-    console.log(incomeValues, totalIncome);
-    console.log(outcomeValues, totalOutcome);
 
     const output = {
-      transactionsTotal: transactions,
-      balance
-    }
+      transactions,
+      balance,
+    };
+
     return response.json(output)
   } catch (err) {
     return response.status(400).json({ error: err.message });
@@ -92,13 +39,8 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
-    const transaction = {
-      id: uuid(),
-      title,
-      value,
-      type
-    }
-    transactions.push(transaction);
+
+    const transaction = transactionsRepository.create({ title, value, type });
 
     return response.json(transaction);
   } catch (err) {
