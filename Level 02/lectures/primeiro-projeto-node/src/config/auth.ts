94acbc5cd6d997994d6ca2f8d789e26646360e21
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-29 20:12:25 -0300

    level 02 add autentication middleware

---
+++ Level 02/lectures/primeiro-projeto-node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'c38b8e83fca9e2ac688287ca9d57df30',
+    expiresIn: '1d',
+  }
+}
--- Level 02/lectures/primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ Level 02/lectures/primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -33,9 +34,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.')
     }
 
-    const token = sign({}, 'c38b8e83fca9e2ac688287ca9d57df30', {
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
