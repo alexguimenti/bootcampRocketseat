bec175e08b346454ef0e0254ea90a034d96226f6
Alexandre Guimenti <alexguimenti@gmail.com>
2020-06-29 09:15:05 -0300

    level 02 add user authentication with JWT

---
+++ Level 02/lectures/primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string
+}
+
+interface Response {
+  user: User
+  token: string
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.')
+    }
+
+    // user.password = senha criptografada
+    // password = senha não-criptografada
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.')
+    }
+
+    const token = sign({}, 'c38b8e83fca9e2ac688287ca9d57df30', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token
+    }
+  }
+}
+
+export default AuthenticateUserService
