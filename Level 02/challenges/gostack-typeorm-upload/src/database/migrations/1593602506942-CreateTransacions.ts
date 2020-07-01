4cfbd7b951c64d197571e14fd0da6867212428ff
Alexandre Guimenti <alexguimenti@gmail.com>
2020-07-01 08:45:30 -0300

    level 02 challenge setup and db init

---
+++ Level 02/challenges/gostack-typeorm-upload/src/database/migrations/1593602506942-CreateTransacions.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransacions1593602506942
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            precision: 2,
+            isNullable: false,
+          },
+          {
+            name: 'category_id',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
---
+++ Level 02/challenges/gostack-typeorm-upload/src/routes/transactions.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+
+// import TransactionsRepository from '../repositories/TransactionsRepository';
+// import CreateTransactionService from '../services/CreateTransactionService';
+// import DeleteTransactionService from '../services/DeleteTransactionService';
+// import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const transactionsRouter = Router();
+
+const transactions = [];
+
+transactionsRouter.get('/', async (request, response) => {
+  return response.json(transactions);
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category_id } = request.body;
+
+  const transaction = {
+    id: uuid(),
+    title,
+    value,
+    type,
+    category_id,
+  };
+
+  transactions.push(transaction);
+
+  return response.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  // TODO
+});
+
+transactionsRouter.post('/import', async (request, response) => {
+  // TODO
+});
+
+export default transactionsRouter;
