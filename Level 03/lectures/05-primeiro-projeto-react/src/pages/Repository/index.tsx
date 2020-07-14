02101b7617fe2f0aa6ccef54f34d22b8f34bf6f7
Alexandre Guimenti <alexguimenti@gmail.com>
2020-07-14 08:42:42 -0300

    level 03 lecture create rpository page

--- Level 03/lectures/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ Level 03/lectures/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/25828420?s=460&u=feac06e502081c1f4fe8687c8d1ead713c501069&v=4"
+            alt="Alexandre"
+          />
+          <div>
+            <strong>Alexandre/Repository</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>23</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>18</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>asdsade</strong>
+            <p>asdasd</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
